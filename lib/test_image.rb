98e4d21d7bd7e0348ca1fbc08e159f7c27667f10
Jairo Diaz <jairo.diaz@alumni.ucd.ie>
2011-05-22 21:05:43 +0100

    added file to lib to retrieve images from the command line

---
+++ lib/test_image.rb
@@ -0,0 +1,55 @@
+require 'rubygems'
+require 'rest_client'
+require 'crack'
+
+patent_number = ARGV[0]
+puts "Patent number is: #{patent_number}"
+url = "http://ops.epo.org/2.6.2/rest-services/published-data/publication/docdb/#{patent_number}/images"
+
+#test patents
+#1 US.2011074386.A1
+#2 EP.1000000.A1
+#3 GB.343434.A
+
+response = RestClient.get(url)
+
+doc = Crack::XML.parse (response)
+
+#@search.last_query = @doc['ops:world_patent_data']['ops:biblio_search']['ops:query']
+
+puts response.body
+puts "----------------------------------------------------------------------------"
+puts doc.inspect
+
+#Page 43 describes the response structure
+image_array = doc["ops:world_patent_data"]["ops:document_inquiry"]["ops:inquiry_result"]["ops:document_instance"]
+
+puts "Image Array is:"
+puts image_array.inspect
+puts "Image Array length: #{image_array.size}"
+
+puts "Image Element types:"
+#Page 48 shows types as: Drawing, FullDocument or FirstPageClipping
+image_element_link = nil
+image_array.each do |image_element|
+  image_element_type = image_element["desc"]
+  puts image_element_type
+  if image_element_type == "FirstPageClipping"
+    image_element_link = image_element["link"]
+    puts "Found FirstPage image at: #{image_element_link}"
+  end
+end
+
+if image_element_link
+  url_for_images = "http://ops.epo.org/2.6.2/rest-services/"
+
+  #This information should be retrieved from the response, here we use a default value
+  name_extension = ".png"
+
+  url = url_for_images + image_element_link + name_extension
+
+  puts "Retrieving image with call to: #{url}"
+  response = RestClient.get(url)
+
+  response.inspect
+end
